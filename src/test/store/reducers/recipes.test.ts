97a40377e9140c14f62f334c072b2b9904c6f7b4
MartinBINARD <martin.binard.dev@gmail.com>
2023-09-11 13:27:11 +0200

    Tests : recipesReducer > action synchrone

--- src/test/store/reducers/recipes.test.ts
+++ src/test/store/reducers/recipes.test.ts
@@ -3,6 +3,7 @@ import { describe, expect, expectTypeOf, it } from 'vitest';
 // j'importe mon unité de code à tester
 // → recipesReducer
 import recipesReducer, {
+  fetchRecipes,
   initialState,
   setLoading,
 } from '../../../store/reducers/recipes';
@@ -30,58 +31,82 @@ describe('Recipes Reducer', () => {
       expect(recipesReducer(undefined, { type: '@@INIT' })).toBe(initialState);
     });
 
-    it('Should handle setLoading', () => {
-      // je génère mon action pour avoir mon objet action
-      const actionLoadingFalse = setLoading(false);
-      // j'appelle mon reducer
-      const stateLoadingFalse = recipesReducer(
-        initialState,
-        actionLoadingFalse
-      );
+    describe('Action', () => {
+      it('Should handle setLoading', () => {
+        // je génère mon action pour avoir mon objet action
+        const actionLoadingFalse = setLoading(false);
+        // j'appelle mon reducer
+        const stateLoadingFalse = recipesReducer(
+          initialState,
+          actionLoadingFalse
+        );
 
-      // expect(stateLoadingFalse).toBe({
-      //   ...initialState,
-      //   loading: false,
-      // }); // FAIL
+        // expect(stateLoadingFalse).toBe({
+        //   ...initialState,
+        //   loading: false,
+        // }); // FAIL
 
-      /*
-        Par contre, attention à la comparaison d'objets :
+        /*
+          Par contre, attention à la comparaison d'objets :
+  
+          const user1 = { name: 'Dave' };
+          const user2 = { name: 'Dave' };
+  
+          const admin = user1;
+  
+          // Comparaison de surface
+          user1 === user2 → false
+          user1 === admin → true
+  
+          // Comparaison en profondeur
+          user1 === user2 → true (ils ont les même valeurs)
+          user1 === admin → true
+  
+          Dans nos tests :
+          expect({ loading: true, list: [], favorites: [] })
+            .toBe(initialState); // FAIL → 2 références différentes
+  
+          expect({ loading: trie, list: [], favorites: [] })
+            .toEqual(initialState); // PASS → même valeurs
+        */
+        expect(stateLoadingFalse).toEqual({
+          ...initialState,
+          loading: false,
+        });
 
-        const user1 = { name: 'Dave' };
-        const user2 = { name: 'Dave' };
+        // on peut aussi tester uniquement la valeur modifiée
+        // je teste la valeur retournée → mon state avec `loading` à `false`
+        expect(stateLoadingFalse.loading).toBe(false);
 
-        const admin = user1;
-
-        // Comparaison de surface
-        user1 === user2 → false
-        user1 === admin → true
-
-        // Comparaison en profondeur
-        user1 === user2 → true (ils ont les même valeurs)
-        user1 === admin → true
-
-        Dans nos tests :
-        expect({ loading: true, list: [], favorites: [] })
-          .toBe(initialState); // FAIL → 2 références différentes
-
-        expect({ loading: trie, list: [], favorites: [] })
-          .toEqual(initialState); // PASS → même valeurs
-      */
-      expect(stateLoadingFalse).toEqual({
-        ...initialState,
-        loading: false,
+        // je teste l'action inverse
+        const actionLoadingTrue = setLoading(true);
+        // j'appelle mon reducer
+        const stateLoadingTrue = recipesReducer(
+          initialState,
+          actionLoadingTrue
+        );
+        // je teste la valeur retournée → mon state avec `loading` à `true`
+        expect(stateLoadingTrue.loading).toBe(true);
       });
+    });
 
-      // on peut aussi tester uniquement la valeur modifiée
-      // je teste la valeur retournée → mon state avec `loading` à `false`
-      expect(stateLoadingFalse.loading).toBe(false);
-
-      // je teste l'action inverse
-      const actionLoadingTrue = setLoading(true);
-      // j'appelle mon reducer
-      const stateLoadingTrue = recipesReducer(initialState, actionLoadingTrue);
-      // je teste la valeur retournée → mon state avec `loading` à `true`
-      expect(stateLoadingTrue.loading).toBe(true);
+    describe('Thunk', () => {
+      // je veux tester si mon reducer gère bien mon thunk `fetchRecipe`
+      // il englobe 3 _action creators_ (fulfilled, rejected, pending)
+      it('Should handle fetchRecipe.fulfilled', () => {
+        // je dois créer mon action → on simule le dispatch
+        // j'ai besoin :
+        //   - payload : données retournées par mon API
+        //     → je peux mettre des infos « bidons » : le principal
+        //       est de retrouver ces infos dans mon state final
+        //   - un `requestId` (généré par Toolkit)
+        //     → je lui donne un faux ID, le principal est de lui en donner un
+        const fakePayload = [{ fakeRecipe: 'testing' }];
+        const fakeRequestId = 'id123456fake';
+        // je génère mon action
+        const action = fetchRecipes.fulfilled(fakePayload, fakeRequestId);
+        // TS râle mais c'est pas grave ici…
+      });
     });
   });
 });
